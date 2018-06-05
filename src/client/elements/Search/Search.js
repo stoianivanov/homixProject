e1a3ad3c01363e91085c069e630fa25262b212d0
mpdincheva <mpdincheva@gmail.com>
2018-06-05 22:26:42 +0300

    Added search component

--- src/client/elements/Search/Search.js
+++ src/client/elements/Search/Search.js
@@ -1,1 +1,19 @@
-// here goes search component
+import React from "react";
+
+const Search = ({
+  isLoading,
+  handleResultSelect,
+  onSearchChange,
+  results,
+  value
+}) => (
+  <Search
+    loading={isLoading}
+    onResultSelect={handleResultSelect}
+    onSearchChange={onSearchChange}
+    results={results}
+    value={value}
+  />
+);
+
+export default Search;
